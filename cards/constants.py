f592bbc8c6bde080ddd2f161465aa93cb21ca4d1
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2016-07-09 15:42:45 +0200

    Add prefix to all non-project specific template fields to avoid naming collisions
    For example, the 'title' field would commonly appear in card data as
    well.
    
    Adding an underscore prefix also makes it easier to distinguish which
    fields are related to your own data and which are not.

--- cards/constants.py
+++ cards/constants.py
@@ -11,28 +11,28 @@ class ColumnDescriptors:
     BACK_ONLY = '@back-only'                    # Descriptor that can be applied to a column to make its contents only be used when generating the back of the card
 
 
-class TemplateFields:
-    # Required fields (system fields)
-    PAGES = 'pages'                             # Field in index.html to be replaced with all generated pages
-    PAGE_NUMBER = 'page_number'                 # Field in page.html to be replaced with current page number
-    PAGES_TOTAL = 'pages_total'                 # Field in page.html to be replaced with total amount of pages
-    CARDS = 'cards'                             # Field in page.html to be replaced with all generated cards for a single page
-    CARD_SIZE = 'card_size'                     # Field in card.html to be replaced with the size-type of the card
-    CARD_CONTENT = 'card_content'               # Field in card.html to be replaced with the generated content of the card
-
-    # Optional fields (user fields)
-    CARD_INDEX = 'card_index'                   # Optional field that is replaced with the index of the card (1 being the first index)
-    CARD_COPY_INDEX = 'card_copy_index'         # Optional field that is replaced with the copy index of the card (1 being the first index). This is the same index for any instance of this card
-    CARD_ROW_INDEX = 'card_row_index'           # Optional field that is replaced with the index of the card in the datasource (2 being the first index, since the CSV header counts as the first row)
-    CARD_TEMPLATE_PATH = 'card_template_path'   # Optional field that is replaced with the path of the template used to generate the card
-    CARDS_TOTAL = 'cards_total'                 # Optional field that is replaced with the total amount of all generated cards
-
-    VERSION = 'version'                         # Optional field that is replaced with the version identifier of the project
-    PROGRAM_VERSION = 'program_version'         # Optional field that is replaced with the version identifier of the program
-    TITLE = 'title'                             # Optional field that is replaced with the title of the project
-    DESCRIPTION = 'description'                 # Optional field that is replaced with the description of the project
-    COPYRIGHT = 'copyright'                     # Optional field that is replaced with the copyright notice for the project
-
-
 class TemplateFieldDescriptors:
     COPY_ONLY = '@copy-only'                    # Descriptor that can be applied to an image-field to prevent it from being transformed into an <img> tag (only keeping the image path and copying the image to the output directory)
+
+
+class TemplateFields:
+    # Required fields (system fields)
+    PAGES = '_pages'                             # Field in index.html to be replaced with all generated pages
+    PAGE_NUMBER = '_page_number'                 # Field in page.html to be replaced with current page number
+    PAGES_TOTAL = '_pages_total'                 # Field in page.html to be replaced with total amount of pages
+    CARDS = '_cards'                             # Field in page.html to be replaced with all generated cards for a single page
+    CARD_SIZE = '_card_size'                     # Field in card.html to be replaced with the size-type of the card
+    CARD_CONTENT = '_card_content'               # Field in card.html to be replaced with the generated content of the card
+
+    # Optional fields (user fields)
+    CARD_INDEX = '_card_index'                   # Optional field that is replaced with the index of the card (1 being the first index)
+    CARD_COPY_INDEX = '_card_copy_index'         # Optional field that is replaced with the copy index of the card (1 being the first index). This is the same index for any instance of this card
+    CARD_ROW_INDEX = '_card_row_index'           # Optional field that is replaced with the index of the card in the datasource (2 being the first index, since the CSV header counts as the first row)
+    CARD_TEMPLATE_PATH = '_card_template_path'   # Optional field that is replaced with the path of the template used to generate the card
+    CARDS_TOTAL = '_cards_total'                 # Optional field that is replaced with the total amount of all generated cards
+
+    VERSION = '_version'                         # Optional field that is replaced with the version identifier of the project
+    PROGRAM_VERSION = '_program_version'         # Optional field that is replaced with the version identifier of the program
+    TITLE = '_title'                             # Optional field that is replaced with the title of the project
+    DESCRIPTION = '_description'                 # Optional field that is replaced with the description of the project
+    COPYRIGHT = '_copyright'                     # Optional field that is replaced with the copyright notice for the project
