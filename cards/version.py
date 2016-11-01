61caab42a23b0b0f407e93d0903d02cb0f76b3c3
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2016-11-01 19:34:36 +0100

    Version 0.6.3

--- cards/version.py
+++ cards/version.py
@@ -6,4 +6,4 @@ This script manages the version identifier for cards.py.
 The version is defined by the Semantic Versioning convention (see http://semver.org).
 """
 
-__version__ = '0.6.2'
+__version__ = '0.6.3'
